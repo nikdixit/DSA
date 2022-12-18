f6ac95902ca6ad7d03afc4c3175b9fe9bc42316c
Nikhil <nikhildixit2125@gmail.com>
2022-12-18 20:13:39 +0530

    Update Main Method + Added some more functions

--- Tress/TreeTransversalUsingPreorder.c
+++ Tress/TreeTransversalUsingPreorder.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 struct node {
+    int key;
     int data;
     struct node *left;
     struct node *right;
@@ -22,6 +23,57 @@ struct node *create(){
     printf("Enter right child of %d\n",x);
     newnode->right=create();
     return newnode;
+}
+
+struct node *deleteNode(struct node *root, int key) {
+  // Return if the tree is empty
+  if (root == NULL) return root;
+
+  // Find the node to be deleted
+  if (key < root->key)
+    root->left = deleteNode(root->left, key);
+  else if (key > root->key)
+    root->right = deleteNode(root->right, key);
+
+  else {
+    // If the node is with only one child or no child
+    if (root->left == NULL) {
+      struct node *temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL) {
+      struct node *temp = root->left;
+      free(root);
+      return temp;
+    }
+
+    // If the node has two children
+    // struct node *temp = minValueNode(root->right);
+
+    // Place the inorder successor in position of the node to be deleted
+    root->key = temp->key;
+
+    // Delete the inorder successor
+    root->right = deleteNode(root->right, temp->key);
+  }
+  return root;
+}
+
+void search(struct node *root,int searchkey){
+    int ncount=0;
+    int ycount=0;
+    if(root==NULL){
+        ncount++;
+        return;
+    }else if (root->data==searchkey){
+        ycount++;
+        return;
+    }else{
+        search(root->left,searchkey);
+        search(root->right,searchkey);
+    }
+    // search(root->left,key);
+    // search(root->right,key);
 }
 
 void preorder(struct node *root){
@@ -54,15 +106,69 @@ void inorder(struct node *root){
 void main(){
     struct node *root;
     root=0;
-    root=create();
-    printf("\n");
-    printf("PreOrder is: ");
-    preorder(root);
-    printf("\n");
-    printf("PostOrder is: ");
-    postorder(root);
-    printf("\n");
-    printf("InOrder is: ");
-    inorder(root);
-    printf("\n");    
+    int ch;
+    while(ch!=5){
+    printf("\nOperations you can perform: \n\t 1.Create \n\t 2.Delete \n\t 3.Display \n\t 4.Search \n\t 5.Exit\n");
+    printf("Enter choice: ");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+    case 1:
+        root=create();
+        break;        
+    case 2 :
+        int key;
+        printf("Enter the key to be searched: ");
+        scanf("%d",&key);    
+        deleteNode(root,key);
+        break;
+    case 3:
+        int index;
+        while(index!=4){ 
+            printf("\nOperations you can perform: \n\t 1.Preorder \n\t 2.Postorder \n\t 3.Inorder \n\t 4.Exit\n");
+            printf("Enter choice: ");
+            scanf("%d",&index);
+        switch (index)
+        {
+        case 1:
+            printf("\n");
+            printf("PreOrder is: ");
+            preorder(root);
+            printf("\n");
+            break;
+        case 2:
+            printf("\n");
+            printf("PosOrder is: ");
+            postorder(root);
+            printf("\n");
+            break;
+        case 3:
+            printf("\n");
+            printf("InOrder is: ");
+            inorder(root);
+            printf("\n");
+            break;
+        case 4:
+            exit(0);
+            break;    
+        default:
+            printf("Invalid choice\n");
+            break;
+        }
+        break;
+    }
+
+    case 4:
+        int searchkey;
+        printf("Enter the key to be searched: ");
+        scanf("%d",&searchkey);
+        search(root,searchkey);
+        break;
+
+    case 5: exit(0);
+        break;
+    default: printf("Invalid choice\n");
+        break;
+        }
+    }
 }
